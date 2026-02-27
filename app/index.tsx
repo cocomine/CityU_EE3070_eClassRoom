b33528ba75cc32141effff29483e4f00d5cd5378
cocomine <roguish.6888467468@gmail.com>
2026-02-27 16:13:16 +0800

    feat: enhance course management with loading states and simulated data fetching

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
-import {View} from "@/components/ui/view";
-import {Text} from "@/components/ui/text";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {Button} from "@/components/ui/button";
-import {StyleSheet, TextInputSubmitEditingEvent, TouchableOpacity} from "react-native";
-import {BottomSheet, useBottomSheet} from '@/components/ui/bottom-sheet';
-import {InputOTP} from "@/components/ui/input-otp";
-import {useCallback, useMemo, useState} from "react";
-import {useRouter} from "expo-router";
-import {Separator} from "@/components/ui/separator";
-import {Input} from "@/components/ui/input";
+import { View } from "@/components/ui/view";
+import { Text } from "@/components/ui/text";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "@/components/ui/button";
+import { StyleSheet, TextInputProps, TextInputSubmitEditingEvent, TouchableOpacity } from "react-native";
+import { BottomSheet, useBottomSheet } from '@/components/ui/bottom-sheet';
+import { InputOTP, InputOTPProps } from "@/components/ui/input-otp";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useRouter } from "expo-router";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { wait } from "@/constants/wait";
+import { LoadingOverlay, Spinner } from "@/components/ui/spinner";
 
 const FakeCourse = [
     {id: '123456', title: 'EE1000'},
@@ -17,8 +20,13 @@ const FakeCourse = [
     {id: '123459', title: 'EE1003'},
     {id: '123460', title: 'EE1004'},
     {id: '123461', title: 'EE1005'},
-    {id: '123462', title: 'EE1006'},
+    {id: '123462', title: 'EE1006'}
 ];
+
+type Course = {
+    id: string;
+    title: string;
+}[]
 
 export default function Root() {
     const studentBottomSheet = useBottomSheet();
@@ -45,90 +53,127 @@ export default function Root() {
 function TeacherBottomSheet({isVisible, onClose}: { isVisible: boolean, onClose: () => void }) {
     const router = useRouter();
     const createNewCourseBottomSheet = useBottomSheet();
-    const [courseList, setCourseList] = useState<typeof FakeCourse>(FakeCourse);
+    const [courseList, setCourseList] = useState<Course | null>(null);
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     // when user click a course, navigate to teacher page with course id and close the bottom sheet
     const onClick = useCallback((courseId: string) => {
-        console.log('Selected:', courseId)
-        router.navigate({pathname: '/teacher', params: {courseId}})
+        console.log('Selected:', courseId);
+        router.navigate({pathname: '/teacher', params: {courseId}});
         onClose();
-    }, [onClose, router])
+    }, [onClose, router]);
 
     // when user click create new course, navigate to create course page and close the bottom sheet
     const onCreateNewCourse = useCallback(() => {
         onClose();
         setTimeout(createNewCourseBottomSheet.open, 400);
-    }, [createNewCourseBottomSheet, onClose])
+    }, [createNewCourseBottomSheet, onClose]);
 
-    // when user complete entering course name, create new course and navigate to teacher page with new course id and close the bottom sheet
-    const onCreateNewCourseComplete = useCallback(({nativeEvent}: TextInputSubmitEditingEvent) => {
-        console.log(nativeEvent.text)
+    // when user complete entering course name, create new course and navigate to teacher page with new course id and
+    // close the bottom sheet
+    const onCreateNewCourseComplete = useCallback<Exclude<TextInputProps['onSubmitEditing'], undefined>>(async ({nativeEvent}) => {
+        console.log(nativeEvent.text);
 
         // validate course name
-        if(!nativeEvent.text || nativeEvent.text.trim() === '') {
-            setError('Course name cannot be empty.')
+        if ( !nativeEvent.text || nativeEvent.text.trim() === '' ) {
+            setError('Course name cannot be empty.');
             return;
         }
 
         //todo: create new course with name nativeEvent.text and get course id
+        setLoading(true);
+        await wait(2000); // Simulate creating course with a delay
         const courseId = '000000';
         const success = true;
 
-        if (success) {
+        if ( success ) {
             // course created successfully, navigate to teacher page with new course id
-            setCourseList(prev => [...prev, {id: courseId, title: nativeEvent.text}])
-            router.navigate({pathname: '/teacher', params: {courseId}})
-            createNewCourseBottomSheet.close()
+            if ( courseList !== null ) {
+                setCourseList([...courseList, {id: courseId, title: nativeEvent.text}]);
+            } else {
+                setCourseList([{id: courseId, title: nativeEvent.text}]);
+            }
+            router.navigate({pathname: '/teacher', params: {courseId}});
+            createNewCourseBottomSheet.close();
         } else {
             // course creation failed, show error message
-            setError('Failed to create course. Please try again.')
+            setError('Failed to create course. Please try again.');
         }
+        setLoading(false);
 
-    }, [createNewCourseBottomSheet, router])
+    }, [courseList, createNewCourseBottomSheet, router]);
 
     // when course name changes, clear error message
     const onChangeText = useCallback(() => {
-        if (error) {
+        if ( error ) {
             setError('');
         }
-    }, [error])
+    }, [error]);
 
     // render course list, when course list changes, re-render the list
     const renderItem = useMemo(() => {
-        return courseList.map((item) => (
+        console.log(courseList);
+        if ( courseList === null ) {
+            return new Array(5).fill(0).map((_, i) => (
+                <View
+                    key={i}
+                    style={[styles.courseList, {gap: 10}]}
+                >
+                    <Skeleton width={'50%'} height={20}/>
+                    <Skeleton width={'30%'} height={14}/>
+                </View>
+            ));
+        }
+        return courseList.sort((itemA, itemB) => {
+            return itemA.title.localeCompare(itemB.title);
+        }).map((item) => (
             <TouchableOpacity
                 key={item.id}
-                style={{
-                    padding: 16,
-                    borderBottomWidth: 1,
-                    borderBottomColor: 'rgba(0,0,0,0.1)',
-                }}
+                style={styles.courseList}
                 onPress={() => onClick(item.id)}
             >
-                <Text variant='body' style={{fontWeight: '600'}}>
+                <Text variant="body" style={{fontWeight: '600'}}>
                     {item.title}
                 </Text>
-                <Text variant='caption' style={{marginTop: 2}}>
+                <Text variant="caption" style={{marginTop: 2}}>
                     {item.id}
                 </Text>
             </TouchableOpacity>
         ));
     }, [onClick, courseList]);
 
+    // fetch course list from server
+    useEffect(() => {
+        //TODO: fetch course list from server and setCourseList
+
+        // Simulate fetching course list from server with a delay
+        setTimeout(() => {
+            setCourseList(FakeCourse);
+            //setCourseList([]) // for testing empty course list
+        }, 5000);
+    }, []);
+
     return (
         <>
             <BottomSheet
                 isVisible={isVisible}
                 onClose={onClose}
-                title='Chose your course'
+                title="Chose your course"
                 snapPoints={[0.6, 0.9]}
             >
                 <View style={{paddingBottom: 100, gap: 10}}>
-                    <Button onPress={onCreateNewCourse}>or Create new course</Button>
+                    <Button onPress={onCreateNewCourse} disabled={courseList === null}>or Create new course</Button>
                     <Separator/>
                     <View>
-                        {renderItem}
+                        {renderItem.length !== 0 ? renderItem :
+                            <Text
+                                variant="caption"
+                                style={{textAlign: 'center', marginTop: 20}}
+                            >
+                                No course found. Please create a new course.
+                            </Text>
+                        }
                     </View>
                 </View>
             </BottomSheet>
@@ -139,75 +184,98 @@ function TeacherBottomSheet({isVisible, onClose}: { isVisible: boolean, onClose:
                 snapPoints={[0.3]}
             >
                 <View style={{gap: 12}}>
-                    <Text variant='body'>Enter course Name</Text>
+                    <Text variant="body">Enter course Name</Text>
                     <Input
-                        variant='outline'
+                        variant="outline"
                         error={error}
                         placeholder={'EE3070'}
                         enterKeyHint={'done'}
+                        disabled={loading}
                         onSubmitEditing={onCreateNewCourseComplete}
                         onChange={onChangeText}
+                        rightComponent={() => loading ? <Spinner size="default" variant="circle"/> : null}
+
                     />
                 </View>
             </BottomSheet>
         </>
-    )
+    );
 }
 
 function StudentBottomSheet({isVisible, onClose}: { isVisible: boolean, onClose: () => void }) {
     const [courseId, setCourseId] = useState('');
     const [error, setError] = useState('');
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     // when course id changes, clear error message
     const onChangeText = useCallback((value: string) => {
         setCourseId(value);
-        if (error) {
+        if ( error ) {
             setError('');
         }
-    }, [error])
+    }, [error]);
 
     // when user complete entering course id, validate it and navigate to student page if valid
-    const onComplete = useCallback<(value: string) => void>((value) => {
-        console.log('Entered course ID:', value)
+    const onComplete = useCallback<Exclude<InputOTPProps["onComplete"], undefined>>(async (value) => {
+        console.log('Entered course ID:', value);
 
         //todo: validate course id
+        setLoading(true);
+        await wait(2000); // Simulate validating course ID with a delay
         const valid = true;
 
-        if (valid) {
+        if ( valid ) {
             // course id is valid, navigate to student page
-            router.navigate({pathname: '/student', params: {courseId: value}})
-            setCourseId('')
+            router.navigate({pathname: '/student', params: {courseId: value}});
+            setCourseId('');
             onClose();
         } else {
             // course id is invalid, show error message
-            setError('Invalid course code. Please try again.')
-            setCourseId('')
+            setError('Invalid course code. Please try again.');
+            setCourseId('');
         }
+        setLoading(false);
 
-    }, [onClose, router])
+    }, [onClose, router]);
 
     return (
-        <BottomSheet
-            isVisible={isVisible}
-            onClose={onClose}
-            title='Enter course code'
-            snapPoints={[0.3]}
-        >
-            <View>
-                <InputOTP
-                    length={6}
-                    value={courseId}
-                    error={error}
-                    onChangeText={onChangeText}
-                    onComplete={onComplete}
+        <>
+            <BottomSheet
+                isVisible={isVisible}
+                onClose={onClose}
+                title="Enter course code"
+                snapPoints={[0.3]}
+            >
+                <View>
+                    <InputOTP
+                        length={6}
+                        value={courseId}
+                        error={error}
+                        disabled={loading}
+                        onChangeText={onChangeText}
+                        onComplete={onComplete}
+                    />
+                </View>
+                <LoadingOverlay
+                    visible={loading}
+                    size='lg'
+                    variant='circle'
+                    label='Loading...'
+                    backdrop={false}
+                    backdropOpacity={0.7}
                 />
-            </View>
-        </BottomSheet>
-    )
+            </BottomSheet>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
+    courseList: {
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(104,104,104,0.1)'
+    },
     container: {
         flex: 1,
         alignItems: 'center',
@@ -223,6 +291,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         width: '100%',
-        paddingHorizontal: 20,
-    },
+        paddingHorizontal: 20
+    }
 });
--- app/student/index.tsx
+++ app/student/index.tsx
@@ -3,15 +3,31 @@ import {View} from "@/components/ui/view";
 import {Text} from "@/components/ui/text";
 import {StyleSheet} from "react-native";
 import {Stack, useLocalSearchParams} from "expo-router";
+import {useEffect, useState} from "react";
+
+const FakeCourse = {title: 'EE1000'};
+
+type CourseDetails = {
+    title: string;
+}
 
 export default function Student() {
     const {courseId} = useLocalSearchParams<{courseId: string}>();
-    console.log(courseId)
+    const [courseDetails, setCourseDetails] = useState<CourseDetails | null>(null);
+
+    useEffect(() => {
+        //TODO: fetch course title from backend
+
+        // Simulate fetching course details from backend with a delay
+        setTimeout(() => {
+            setCourseDetails({title: `Course Title for ${courseId}`});
+        }, 1000);
+    }, [courseId])
 
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Text variant={'title'}>Student Mode</Text>
+                <Text variant={'title'}>{courseDetails?.title}</Text>
                 <Text variant={'subtitle'}>{courseId}</Text>
             </View>
         </SafeAreaView>
